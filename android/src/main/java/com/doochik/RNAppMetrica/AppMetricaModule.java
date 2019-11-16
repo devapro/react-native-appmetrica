4fe3f4152c362810d9a9a787f510a3a583e72dab
Arseniy Harlanow <arsenyzp@gmail.com>
2019-11-16 15:45:36 +0300

    Update AppMetricaModule.java

--- android/src/main/java/com/doochik/RNAppMetrica/AppMetricaModule.java
+++ android/src/main/java/com/doochik/RNAppMetrica/AppMetricaModule.java
@@ -2,8 +2,9 @@ package com.doochik.RNAppMetrica;
 
 import android.app.Activity;
 import android.app.Application;
-import android.support.annotation.Nullable;
 import android.util.Log;
+
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
